f3d8a563abf03d7113823d088b0c6da1b054f982
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-05-28 09:50:58 -0300

    Add files via upload

---
+++ SQL/intregacao.sql
@@ -0,0 +1,11 @@
+create database cadastro1;
+use cadastro1;
+
+create table if not exists cliente1(
+CPF VARCHAR(11) NOT NULL PRIMARY KEY,
+PRIMEIRO_NOME VARCHAR(50) NOT NULL,
+SOBRENOME VARCHAR(50) NOT NULL,
+IDADE INT NOT NULL
+);
+
+SELECT * FROM cliente1;
