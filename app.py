44772b7c7ddbd5be39343a15542bb0a1b2416f32
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-06-18 11:50:58 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -16,21 +16,35 @@ bd_config = {
 }
 
 # Criação de rota para o arquivo HTML principal
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/cadastrar', methods=['POST'])
 def criar_cadastro():
-    cpf = request.form['cpf']
-    primeiro_nome = request.form['primeiro_nome']
-    sobrenome = request.form['sobrenome']
-    idade = request.form['idade']
 
-    # Criando a conexão com o banco de dados
-    conectar = mysql.connector.connect(**bd_config)
+    try:
+        cpf = request.form['cpf']
+        primeiro_nome = request.form['primeiro_nome']
+        sobrenome = request.form['sobrenome']
+        idade = request.form['idade']
 
-    # Leva as instruções do SQL até o Python
-    transporte = conectar.cursor()
+        # Criando a conexão com o banco de dados
+        conectar = mysql.connector.connect(**bd_config)
 
-    query = "INSERT INTO cliente1 (CPF, PRIMEIRO_NOME, SOBRENOME, IDADE) VALUES (%s, %s, %s, %s)"
+        # Leva as instruções do SQl do Python até o banco de dados
+        transporte = conectar.cursor()
+
+        query = "INSERT INTO cliente1 (CPF, PRIMEIRO_NOME, SOBRENOME, IDADE) VALUES (%s, %s, %s, %s)"
+        transporte.execute(query(cpf, primeiro_nome, sobrenome, idade))
+        # salva alteração
+        transporte.commit()
+        # fecha o cursor
+        transporte.close()
+        # fecha a conexão do banco de dados
+        conectar.close()
+    except mysql.connector.Error as err:
+        return f"Erro ao gravar no Banco: {err}"
