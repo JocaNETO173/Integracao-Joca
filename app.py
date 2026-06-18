6ae6e56e2c0b9e7d03fb7e0a84c5e9ff610b4097
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-06-18 09:51:41 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,13 +2,35 @@
 # import do render_template para a leitura HTML
 # request para captura de dados
 
-from flask import Flask, render_template, required
+from flask import Flask, render_template, request
 # biblioteca para criar conexão com mysql
 import mysql.connector
 
+app = Flask(__name__)
+
 bd_config = {
-    'host': '127.0.0.1',
+    'host': 'localhost',
     'user': 'root',
     'password': 'escola',
     'database': 'cadastro1'
 }
+
+# Criação de rota para o arquivo HTML principal
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/cadastrar', methods=['POST'])
+def criar_cadastro():
+    cpf = request.form['cpf']
+    primeiro_nome = request.form['primeiro_nome']
+    sobrenome = request.form['sobrenome']
+    idade = request.form['idade']
+
+    # Criando a conexão com o banco de dados
+    conectar = mysql.connector.connect(**bd_config)
+
+    # Leva as instruções do SQL até o Python
+    transporte = conectar.cursor()
+
+    query = "INSERT INTO cliente1 (CPF, PRIMEIRO_NOME, SOBRENOME, IDADE) VALUES (%s, %s, %s, %s)"
